d162aa8cbc7d036f2b655c662c45fb279b62c3c2
mariquegonn-dev <ocatodev@gmail.com>
2023-08-11 08:29:52 -0300

    config: tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      whitePrimary: "#EDEDED",
-      graySec: "#A0A0A0",
-      purplePrimary: "#9933F8",
+      colors: {
+        whitePrimary: "#EDEDED",
+        graySec: "#A0A0A0",
+        purplePrimary: "#9933F8",
+      },
     },
   },
   plugins: [],
