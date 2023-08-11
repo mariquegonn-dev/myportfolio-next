d0237d10e44db489b467230a34a699238f8ebc93
mariquegonn-dev <ocatodev@gmail.com>
2023-08-11 08:31:05 -0300

    feat: Hero created

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export const Hero = () => {
+  return (
+    <>
+      <section className="container mx-auto flex min-h-[80vh] items-center justify-center px-4">
+        <h1 className="text-5xl leading-tight text-whitePrimary">
+          Olá! Me chamo Henrique, prazer,{<br />} e sou desenvolvedor Front-end;
+          {<br />}versado em {<span className="text-purplePrimary">React</span>}{" "}
+          e {<span className="text-purplePrimary">Typescript</span>}.
+        </h1>
+      </section>
+      <div className="mx-auto max-w-max animate-bounce cursor-pointer p-4">
+        <Image
+          src="/assets/svg/down.svg"
+          width={48}
+          height={48}
+          alt="down button"
+        />
+      </div>
+    </>
+  );
+};
