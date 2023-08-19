bae528c3eb0ddf9c13aca81ba55e543ccb3578df
mariquegonn-dev <ocatodev@gmail.com>
2023-08-19 19:17:29 -0300

    feat: Hero modified

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <>
-      <section className="container mx-auto flex min-h-[80vh] items-center justify-center px-4">
+    <section>
+      <div className="container mx-auto flex min-h-[80vh] items-center justify-center px-4">
         <h1 className="text-5xl leading-tight text-whitePrimary">
           Olá! Me chamo Henrique, prazer,{<br />} e sou desenvolvedor Front-end;
           {<br />}versado em {<span className="text-purplePrimary">React</span>}{" "}
           e {<span className="text-purplePrimary">Typescript</span>}.
         </h1>
-      </section>
+      </div>
       <div className="z-10 mx-auto max-w-max animate-bounce cursor-pointer p-4">
         <Image
           src="/assets/svg/down.svg"
@@ -18,6 +18,6 @@ export const Hero = () => {
           alt="down button"
         />
       </div>
-    </>
+    </section>
   );
 };
